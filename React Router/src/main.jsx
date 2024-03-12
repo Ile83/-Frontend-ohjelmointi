bd09bad8ee3df88230baa2a383990494a196b80f
Ile83 <ilkka@heusala.fi>
2024-03-12 13:06:15 +0200

    Home is not defined

---
+++ React Router/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App';
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [                       // children are nested routes with a route
+      {
+        element: <Home />,
+        index: true                   // index route does not need any path
+      },
+      {
+        path: "about",                // path can be defined relative to the parent path
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+
